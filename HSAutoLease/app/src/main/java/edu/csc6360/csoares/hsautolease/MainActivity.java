d3e26b6a89784b8deec4336fc510472b1d104625
Carlos Soares <48662028+carlossoares1@users.noreply.github.com>
2019-07-10 10:32:48 -0400

    AutoLease Draft

---
+++ HSAutoLease/app/src/main/java/edu/csc6360/csoares/hsautolease/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csc6360.csoares.hsautolease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
